0fc782c71217d5317229d82ab64e7c71cbb6fe1e
Lakshmi Priyanka Annapureddy <144724309+PriyankaaAnnapureddy@users.noreply.github.com>
2024-12-06 13:05:22 -0500

    initial setup
    initial setup

---
+++ smart-home-automation/src/main/java/com/example/smart_home_automation/SmartHomeAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_home_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeAutomationApplication.class, args);
+	}
+
+}
